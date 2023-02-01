4b883afe8a337fe41f6fb618cd3e9c4b3b3afb19
Bill Kable <bkable@gmail.com>
2023-02-01 14:30:15 -0600

    <rest-controller-intro-start> start of workshop

---
+++ src/test/java/academy/spring/sample/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.spring.sample.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
